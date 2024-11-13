fb32f0a319a579021252ea67b84a88606438627a
sakshi kesharwani <sakshikesharwani14@gmail.com>
2024-11-13 20:59:46 +0530

    new change

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,33 +3,37 @@ import React from 'react';
 function Card() {
   return (
     <div
-      className="flex gap-4 border w-[50%] h-[35%] p-2 rounded-md bg-white"
+      className="flex gap-4 sm:flex-row flex-col border w-[90vw] h-auto sm:h-[50vh] sm:w-[80vw] lg:h-[42vh] lg:w-[70vw] xl:h-[45vh] xl:w-[60vw]  2xl:w-[50vw] p-2 rounded-md "
       style={{
         boxShadow: "0 6px 12px rgba(0, 0, 0, 0.15), 0 6px 20px rgba(0, 0, 0, 0.10)"
       }}
     >
-      <div className="h-full w-[35%] object-cover border rounded-md overflow-hidden">
+      <div className="sm:h-full sm:w-[35%] w-full h-[250px] border rounded-md overflow-hidden object-cover">
         <img
           src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_640.jpg"
           alt="news img"
-          className="h-full w-full"
+          className="h-full w-full object-cover"
         />
       </div>
-      <div className='flex flex-col justify-start flex-1 gap-3'>
+      <div className="flex flex-col flex-1 gap-3 ">
         <div>
-          <p className="text-[1.5rem] font-light leading-tight text-slate-900">
+          <p className="text-[1.2rem] sm:text-[1.3rem] font-light leading-tight text-slate-900">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus eum laudantium eius similique adipisci.
           </p>
-          <p className="text-[0.84rem]  font-light text-slate-600">
-            <span className='font-bold'> short by </span> Ipsum dolor sit amet consectetur adipisicing elit./ dfdf
+          <p className="text-[0.84rem] font-light text-slate-600">
+            <span className="font-bold"> short by </span> Ipsum dolor sit amet consectetur adipisicing elit.
           </p>
         </div>
-        <p className="text-gray-800 text-[1rem] font-light ">
-          88e3be1793c1492db925cb6940289166 Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore natus ullam a hic molestiae nemo numquam similique distinctio cum ipsam harum, reiciendis, nostrum architecto obcaecati nihil voluptate voluptas iure. Tempora, ipsum doloribus?lorem14
-          lorem14
-          lorem12
+        <div className='flex flex-col justify-between  h-full'>
+        <p className="text-gray-800 text-[0.8rem] sm:text-[0.9rem] font-light">
+          88e3be1793c1492db925cb6940289166 Lorem ipsum dolor sit amet consectetur Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo cupiditate quaerat fugit!lorem18
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae rerum facere odit aspernatur modi.
         </p>
-        <p className='text-[0.84rem]  text-slate-900 font-medium'>Read more at </p>
+        <p className="text-[0.84rem] text-slate-900 font-medium">
+          Read more at
+        </p>
+        </div>
+        
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Navbar() {
+  const [showmenu, setshowmenu] = useState(false);
+  const categories = [
+    'business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'
+  ];
+
   return (
     <div
       className="flex h-[4.5rem] items-center px-3 py-2 "
       style={{ boxShadow: "0 2px 8px 1px rgba(0, 0, 0, 0.21)" }}
     >
-      <div className="flex flex-row h-full p-1 items-center">
+      <div className="flex flex-row h-full p-1 items-center cursor-pointer" onClick={() => setshowmenu(!showmenu)}>
         <img
           src="https://www.svgrepo.com/show/312300/hamburger-menu.svg"
           alt="hamimg"
@@ -14,8 +19,20 @@ function Navbar() {
         />
         <p className="text-[1rem]">Menu</p>
       </div>
-      
+
       <h1 className="text-[2rem] font-semibold flex-1 text-center text-slate-600">News App</h1>
+
+      
+      {showmenu && (
+        <div className="fixed top-0 left-0 h-full w-[60vw] sm:w-[30vw] bg-gray-800 text-ivory p-6 z-10">
+          <button className="text-white" onClick={() => setshowmenu(false)}>Close</button>
+          <div className="flex flex-col gap-6 mt-6 text-[ivory]">
+            {categories.map((item) => (
+              <p key={item} className="text-lg capitalize">{item}</p>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
